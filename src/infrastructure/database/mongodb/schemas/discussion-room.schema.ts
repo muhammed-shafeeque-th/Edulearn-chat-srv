1700ef60df4d1370ba21b597bec84a080a3cec17
shafeeque <shafeeqsha06@gmail.com>
2026-03-21 21:58:07 +0530

    feat(discussion): add discussion room mondogb schema defenition

---
+++ src/infrastructure/database/mongodb/schemas/discussion-room.schema.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: true, collection: 'discussion_rooms' })
+export class DiscussionRoomDocument extends Document {
+  @Prop({ required: true })
+  _id: string;
+
+  @Prop({ required: true, unique: true, index: true })
+  courseId: string;
+
+  @Prop({ required: true, index: true })
+  instructorId: string;
+
+  @Prop({ type: String, default: null })
+  lastMessageId?: string;
+
+  @Prop({ type: Number, default: 0 })
+  lastMessageSequence: number;
+
+  @Prop() createdAt: Date;
+  @Prop() updatedAt: Date;
+}
+
+export const DiscussionRoomSchema = SchemaFactory.createForClass(
+  DiscussionRoomDocument,
+);
+
+DiscussionRoomSchema.index({ courseId: 1 }, { unique: true });
